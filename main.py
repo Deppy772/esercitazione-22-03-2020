4d52fe9c52d14dd80db5f672d9371327741cf10d
Deppy772 <danideppy070104@gmail.com>
2020-03-30 12:01:38 +0200

    Aggiunto minore o maggiore su mani.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from random import randint
 a = randint(1, 100)
-forse = true
+forse = True
 while forse:
 	num = int(input("Che numero ho pensato? "))
+	if num != a:
+		if num > a:
+			print("Il numero a cui ho pensato e' piu' piccolo")
+		else:
+			print("Il numero a cui ho pensa e' piu' grande")
+	else:
+		print("Bravo!!! Hai infovinato")
+		num = False
