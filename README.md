fc98c90e215bc68c08cee9590f785229bc0507ff
Deppy772 <danideppy070104@gmail.com>
2020-03-30 12:13:36 +0200

    Cambiato readme.md e messo a posto gli errori nel codice main.py

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Daniele De Pietri
 Buongiorno a tutti,
-come state? Questa repository contiene
+come state? Questa repository contiene:
+- il seguente file README.md
+- il file .gitignore contenente chiave.secret e password.secret
+- un file python chiamato main.py, il quale genera un numero randomico tra 1 a 100 e tu, caro lettore, sei invitato a infìdovinarlo nel minor numero possibile di tentativi
--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 from random import randint
 a = randint(1, 100)
 forse = True
-<<<<<<< HEAD
-while forse:
-	num = int(input("Che numero ho pensato? "))
-	if num != a:
-=======
 count = 0
 while forse:
 	num = int(input("Che numero ho pensato? "))
 	if num != a:
 		count += 1
->>>>>>> feature
 		if num > a:
 			print("Il numero a cui ho pensato e' piu' piccolo")
 		else:
 			print("Il numero a cui ho pensa e' piu' grande")
 	else:
-<<<<<<< HEAD
 		print("Bravo!!! Hai infovinato")
-=======
 		print("Bravo!!! Hai infovinato in {} tentativi".format(count))
->>>>>>> feature
-		num = False
+		forse = False
