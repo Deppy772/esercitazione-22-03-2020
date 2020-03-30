e910ea77670b8e4357c3fa3a492cf9f8c81322ab
Deppy772 <danideppy070104@gmail.com>
2020-03-30 12:06:30 +0200

    Aggiunto count a main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from random import randint
 a = randint(1, 100)
-forse = true
+forse = True
+count = 0
 while forse:
 	num = int(input("Che numero ho pensato? "))
+	if num != a:
+		count += 1
+		if num > a:
+			print("Il numero a cui ho pensato e' piu' piccolo")
+		else:
+			print("Il numero a cui ho pensa e' piu' grande")
+	else:
+		print("Bravo!!! Hai infovinato in {} tentativi".format(count))
+		num = False
